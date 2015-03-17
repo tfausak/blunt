3f1b0987d4f82c1faedd87225e87b7d1a26f7b5a
Taylor Fausak <taylor@fausak.me>
2015-03-17 13:38:24 -0500

    Parrot the "input" parameter back

--- library/Blunt.hs
+++ library/Blunt.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Blunt where
 
-import Data.ByteString.Lazy (empty)
+import Data.ByteString.Lazy (fromStrict)
 import Network.HTTP.Types (ok200)
-import Network.Wai (Application, responseLBS)
+import Network.Wai (Application, queryString, responseLBS)
 import Network.Wai.Handler.Warp (run)
 
 main :: IO ()
 main = run 8080 application
 
 application :: Application
-application _request respond =
-    let status = ok200
-        headers = []
-        body = empty
+application request respond = do
+    let params = queryString request
+        input = case lookup "input" params of
+            Just (Just param) -> param
+            _ -> ""
+        status = ok200
+        headers = [("Content-Type", "text/plain")]
+        body = fromStrict input
         response = responseLBS status headers body
-    in  respond response
+    respond response
