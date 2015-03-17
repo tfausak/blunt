76e4060f7dee4cf582cd36385298ac4da9670229
Taylor Fausak <taylor@fausak.me>
2015-03-17 08:27:51 -0500

    Create a trivial endpoint

--- library/Blunt.hs
+++ library/Blunt.hs
@@ -1,4 +1,17 @@
 module Blunt where
 
+import Data.ByteString.Lazy (empty)
+import Network.HTTP.Types (ok200)
+import Network.Wai (Application, responseLBS)
+import Network.Wai.Handler.Warp (run)
+
 main :: IO ()
-main = return ()
+main = run 8080 application
+
+application :: Application
+application _request respond =
+    let status = ok200
+        headers = []
+        body = empty
+        response = responseLBS status headers body
+    in  respond response
