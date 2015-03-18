e46d714ebd7e464d6b6218173607efbda31660a4
Taylor Fausak <taylor@fausak.me>
2015-03-18 08:58:01 -0500

    Add some rudimentary routing

--- library/Blunt.hs
+++ library/Blunt.hs
@@ -5,25 +5,26 @@ module Blunt where
 import Data.ByteString.Char8 (unpack)
 import Data.ByteString.Lazy (fromStrict)
 import Data.ByteString.Lazy.Char8 (pack)
-import Network.HTTP.Types (ok200)
-import Network.Wai (Application, queryString, responseLBS)
+import Network.HTTP.Types (notFound404, ok200)
+import Network.Wai (queryString, pathInfo, requestMethod, responseLBS)
 import Network.Wai.Handler.Warp (run)
 import Pointfree (pointfree')
 
 main :: IO ()
-main = run 8080 application
-
-application :: Application
-application request respond = do
-    let params = queryString request
-        input = case lookup "input" params of
-            Just (Just param) -> param
-            _ -> ""
-        maybeOutput = pointfree' (unpack input)
-        status = ok200
-        headers = [("Content-Type", "text/plain")]
-        body = case maybeOutput of
-            Just output -> pack output
-            Nothing -> fromStrict input
-        response = responseLBS status headers body
+main = run 8080 $ \ request respond -> do
+    let method = requestMethod request
+        path = pathInfo request
+        response = case (method, path) of
+            ("GET", []) -> responseLBS ok200 [("Content-Type", "text/html")] "TODO"
+            ("GET", ["pointfree"]) ->
+                let params = queryString request
+                    input = case lookup "input" params of
+                        Just (Just param) -> param
+                        _ -> ""
+                    maybeOutput = pointfree' (unpack input)
+                    body = case maybeOutput of
+                        Just output -> pack output
+                        Nothing -> fromStrict input
+                in  responseLBS ok200 [] body
+            _ -> responseLBS notFound404 [] ""
     respond response
