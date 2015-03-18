fb5439d1db4309c567fff2b4ba55dce7de9dc72b
Taylor Fausak <taylor@fausak.me>
2015-03-18 08:29:00 -0500

    Return the pointfree version of the expression

--- library/Blunt.hs
+++ library/Blunt.hs
@@ -2,10 +2,13 @@
 
 module Blunt where
 
+import Data.ByteString.Char8 (unpack)
 import Data.ByteString.Lazy (fromStrict)
+import Data.ByteString.Lazy.Char8 (pack)
 import Network.HTTP.Types (ok200)
 import Network.Wai (Application, queryString, responseLBS)
 import Network.Wai.Handler.Warp (run)
+import Pointfree (pointfree')
 
 main :: IO ()
 main = run 8080 application
@@ -16,8 +19,11 @@ application request respond = do
         input = case lookup "input" params of
             Just (Just param) -> param
             _ -> ""
+        maybeOutput = pointfree' (unpack input)
         status = ok200
         headers = [("Content-Type", "text/plain")]
-        body = fromStrict input
+        body = case maybeOutput of
+            Just output -> pack output
+            Nothing -> fromStrict input
         response = responseLBS status headers body
     respond response
