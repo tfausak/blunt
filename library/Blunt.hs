959e5864741947ad432793b5a6df9fc1d887c4ef
Taylor Fausak <taylor@fausak.me>
2015-03-18 13:16:14 -0500

    Move HTML into a separate file

--- library/Blunt.hs
+++ library/Blunt.hs
@@ -2,53 +2,28 @@
 
 module Blunt where
 
+import Paths_blunt (getDataFileName)
+
 import Data.ByteString.Char8 (unpack)
 import Data.ByteString.Lazy (fromStrict)
 import Data.ByteString.Lazy.Char8 (pack)
 import Network.HTTP.Types (notFound404, ok200)
-import Network.Wai (queryString, pathInfo, requestMethod, responseLBS)
+import Network.Wai (queryString, pathInfo, requestMethod, responseFile,
+    responseLBS)
 import Network.Wai.Handler.Warp (run)
 import Pointfree (pointfree')
 
 main :: IO ()
 main = run 8080 $ \ request respond -> do
+    index <- getDataFileName "index.html"
     let method = requestMethod request
         path = pathInfo request
         response = case (method, path) of
-            ("GET", []) -> responseLBS ok200 [("Content-Type", "text/html; charset=utf-8")] $ pack $ unlines
-                [ "<!doctype html>"
-                , ""
-                , "<html>"
-                , "  <head>"
-                , "    <title>Blunt</title>"
-                , "  </head>"
-                , ""
-                , "  <body>"
-                , "    <input id='input' autofocus>"
-                , "    <input id='output' readonly>"
-                , ""
-                , "    <script>"
-                , "      'use strict';"
-                , ""
-                , "      (function () {"
-                , "        var input = document.getElementById('input');"
-                , "        var output = document.getElementById('output');"
-                , ""
-                , "        input.oninput = function (event) {"
-                , "          var request = new XMLHttpRequest();"
-                , "          request.onreadystatechange = function () {"
-                , "            if (request.readyState === 4 && request.status === 200) {"
-                , "              output.value = request.response;"
-                , "            }"
-                , "          };"
-                , "          request.open('GET', '/pointfree?input=' + encodeURIComponent(input.value));"
-                , "          request.send();"
-                , "        };"
-                , "      }());"
-                , "    </script>"
-                , "  </body>"
-                , "</html>"
-                ]
+            ("GET", []) -> responseFile
+                ok200
+                [("Content-Type", "text/html; charset=utf-8")]
+                index
+                Nothing
             ("GET", ["pointfree"]) ->
                 let params = queryString request
                     input = case lookup "input" params of
