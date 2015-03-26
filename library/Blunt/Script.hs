7375b7b6c28447258694232bfa14eb462dc9becd
Taylor Fausak <taylor@fausak.me>
2015-03-25 23:39:08 -0500

    Rewrite JS with JMacro

--- library/Blunt/Script.hs
+++ library/Blunt/Script.hs
@@ -1,44 +1,42 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Blunt.Script where
 
-script :: String
-script = unlines js
+import Language.Javascript.JMacro
 
-js :: [String]
-js =
-    [ "'use strict';"
-    , ""
-    , "(function () {"
-    , "  var input = document.getElementById('input');"
-    , "  var pointfree = document.getElementById('pointfree');"
-    , "  var pointful = document.getElementById('pointful');"
-    , ""
-    , "  var updateHash = function () {"
-    , "    window.location.replace('#input=' + input.value);"
-    , "  };"
-    , ""
-    , "  var updateOutput = function () {"
-    , "    var request = new XMLHttpRequest();"
-    , ""
-    , "    request.onreadystatechange = function () {"
-    , "      if (request.readyState === 4 && request.status === 200) {"
-    , "        var response = JSON.parse(request.response);"
-    , ""
-    , "        pointfree.textContent = response.pointfree.join('\\n');"
-    , "        pointful.textContent = response.pointful;"
-    , "      }"
-    , "    };"
-    , "    request.open('GET', '/convert?input=' + encodeURIComponent(input.value));"
-    , "    request.send();"
-    , "  };"
-    , ""
-    , "  input.oninput = function (_event) {"
-    , "    updateHash();"
-    , "    updateOutput();"
-    , "  };"
-    , ""
-    , "  if (window.location.hash.indexOf('#input=') === 0) {"
-    , "    input.value = window.location.hash.substring(7);"
-    , "    input.oninput();"
-    , "  }"
-    , "}());"
-    ]
+script :: String
+script = show (renderJs js)
+
+js :: JStat
+js = [jmacro|
+    var input = document.getElementById("input");
+    var pointfree = document.getElementById("pointfree");
+    var pointful = document.getElementById("pointful");
+
+    var updateHash = \ { window.location.replace("#input=" + input.value); };
+
+    var updateOutput = \ {
+        var request = new XMLHttpRequest;
+
+        request.onreadystatechange = \ {
+            if (request.readyState !== 4 || request.status !== 200) { return; }
+
+            var response = JSON.parse request.response;
+            pointfree.textContent = response.pointfree.join("\n");
+            pointful.textContent = response.pointful;
+        };
+
+        request.open("GET", "/convert?input=" + encodeURIComponent(input.value));
+        request.send();
+    };
+
+    input.oninput = \ {
+        updateHash();
+        updateOutput();
+    };
+    
+    if (window.location.hash.indexOf("#input=") === 0) {
+        input.value = window.location.hash.substring(7);
+        input.oninput();
+    }
+|]
