c0be7b11089bd2609baf69a6f2d43fc0f9d2fdc4
Taylor Fausak <taylor@fausak.me>
2015-03-27 16:16:15 -0500

    Enable request logging

--- library/Blunt/Middleware.hs
+++ library/Blunt/Middleware.hs
@@ -2,6 +2,7 @@ module Blunt.Middleware where
 
 import Network.Wai (Middleware)
 import Network.Wai.Middleware.Gzip (def, gzip)
+import Network.Wai.Middleware.RequestLogger (logStdout)
 
 middleware :: Middleware
-middleware = gzip def
+middleware = gzip def . logStdout
