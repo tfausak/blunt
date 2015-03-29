ddc62003f1dd8a8268f794306907471db50bef84
Taylor Fausak <taylor@fausak.me>
2015-03-28 19:56:36 -0500

    Create an empty websockets application

--- library/Blunt/Application.hs
+++ library/Blunt/Application.hs
@@ -3,9 +3,17 @@ module Blunt.Application where
 import Blunt.Middleware (middleware)
 import Blunt.Router (route)
 import Network.Wai (Application)
+import Network.Wai.Handler.WebSockets (websocketsOr)
+import Network.WebSockets (ServerApp, defaultConnectionOptions)
 
 application :: Application
-application = middleware httpApplication
+application = websocketsOr
+    defaultConnectionOptions
+    wsApplication
+    (middleware httpApplication)
+
+wsApplication :: ServerApp
+wsApplication _pending = return ()
 
 httpApplication :: Application
 httpApplication request respondWith = do
